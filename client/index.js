9f0b41c7a78216ec9636f8f50de14da2a0fcd3c7
Hyo Kim <hyo.kim968@gmail.com>
2021-11-12 18:46:56 -0800

    removed autoplay

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Atomy Platform Business</h1>'
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/gQCVSVRz_uU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/gQCVSVRz_uU" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
       </div>
     );
