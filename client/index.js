5d05949c4f285bcdc90a805b1e320dadb21299b1
Hyo Kim <hyo.kim968@gmail.com>
2021-11-12 18:35:29 -0800

    added atomy video

--- client/index.js
+++ client/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <div>Atomy Platform Business</div>;
+    return (
+      <div>
+        <h1>Atomy Platform Business</h1>'
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/gQCVSVRz_uU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+      </div>
+    );
   }
 }
 
