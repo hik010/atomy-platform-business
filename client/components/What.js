af2820c71f0539da83c66bbc01e9ee48b97eed66
Hyo Kim <hyo.kim968@gmail.com>
2021-11-28 12:58:31 -0800

    new sponser id

--- client/components/What.js
+++ client/components/What.js
@@ -28,10 +28,12 @@ const What = () => {
       <h2>How to Join Us:</h2>
       <p>1. Click Join Us Button on the top</p>
       <p>2. Enter the following credentials</p>
-      <p style={{paddingLeft: '1.5rem'}}>ID: 1233567</p>
+      <p style={{paddingLeft: '1.5rem'}}>ID: 12533567</p>
       <p style={{paddingLeft: '1.5rem'}}>Password: 123456</p>
-      <p>3. Please contact me for more info and Sponser ID</p>
-      <p style={{paddingLeft: '1.5rem'}}>weChat ID / KakaoTalk ID: yskimgs787</p>
+      <p>3. Please choose one of the following Sponser IDs</p>
+      <p style={{paddingLeft: '1.5rem'}}> 29372598, 25788257, 26579684</p>
+      <b> Contact for more info</b>
+      <p style={{paddingLeft: '1.5rem'}}>WeChat ID / KakaoTalk ID: yskimgs787</p>
       <p style={{paddingLeft: '1.5rem'}} >Email: atomykys0426@gmail.com</p>
     </div>
   );
